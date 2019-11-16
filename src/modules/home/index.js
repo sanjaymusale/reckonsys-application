96b89a558ea5fdf77a13205bb118ad7e903c64da
sanjaymusale.10@gmail.com <sanjaymusale.10@gmail.com>
2019-11-16 11:41:29 +0530

    fixed home page

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Jumbotron } from 'reactstrap';
 import './home.scss';
 
-const Example = () => {
-  return (
-    <div>
-      <Jumbotron className="home-container">
-        <h1 className="display-3">Welcome !!!</h1>
-        <p className="lead">This is a demo application built on ReactJS</p>
-        <hr className="my-2" />
-        <p className="lead">Login : test@gmail.com</p>
-        <p className="lead">Password : Test@123</p>
-      </Jumbotron>
-    </div>
-  );
-};
+const Example = props => (
+  <div>
+    <Jumbotron className="home-container">
+      <h1 className="display-3">Welcome !!!</h1>
+      <p className="lead">This is a demo application built on ReactJS</p>
+      <hr className="my-2" />
+      {!props.user.isAuthenticated && (
+        <>
+          <p className="lead">Login : test@gmail.com</p>
+          <p className="lead">Password : Test@123</p>
+        </>
+      )}
+    </Jumbotron>
+  </div>
+);
 
-export default Example;
+const mapStateToProps = state => {
+  return {
+    user: state.users
+  };
+};
+export default connect(mapStateToProps)(Example);
